93244632cd879737ca7356429d3f0cfab8341e58
zsy61314 <383760136@qq.com>
2014-01-07 12:52:33 +0800

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Mac Pro on 14-1-7.
+//  Copyright (c) 2014年 Anson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
